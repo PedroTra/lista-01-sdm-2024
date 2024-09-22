157c61f563139a4211eb7458086f142aa01a7bc3
PedroTra <151466351+PedroTra@users.noreply.github.com>
2024-09-22 17:14:03 -0300

    Create universidade.java

---
+++ universidade.java
@@ -0,0 +1,32 @@
+public class Universidade {
+    private static String Nome;
+    private static String CNPJ;
+    private static String Endereço;
+    private static int QtdeSalas;
+
+    public String getNome() {
+        return Nome;
+    }
+    public void setNome(String nome) {
+        Nome = nome;
+    }
+    public String getCNPJ() {
+        return CNPJ;
+    }
+    public void setCNPJ(String cNPJ) {
+        CNPJ = cNPJ;
+    }
+    public String getEndereço() {
+        return Endereço;
+    }
+    public void setEndereço(String endereço) {
+        Endereço = endereço;
+    }
+    public int getQtdeSalas() {
+        return QtdeSalas;
+    }
+    public void setQtdeSalas(int qtdeSalas) {
+        QtdeSalas = qtdeSalas;
+    }
+
+}
