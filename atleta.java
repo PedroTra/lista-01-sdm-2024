38c817aff02342298eee2b6831c4a15abb7c308a
PedroTra <151466351+PedroTra@users.noreply.github.com>
2024-09-22 17:15:24 -0300

    Create atleta.java

---
+++ atleta.java
@@ -0,0 +1,25 @@
+public class Atleta {
+    private static String Nome;
+    private static String Pais;
+    private static int QtdeMedalhas;
+
+    public String getPais() {
+        return Pais;
+    }
+    public void setPais(String pais) {
+        Pais = pais;
+    }
+    public int getQtdeMedalhas() {
+        return QtdeMedalhas;
+    }
+    public void setQtdeMedalhas(int qtdeMedalhas) {
+        QtdeMedalhas = qtdeMedalhas;
+    }
+    public String getNome() {
+        return Nome;
+    }
+    public void setNome(String nome) {
+        Nome = nome;
+    }
+
+}
